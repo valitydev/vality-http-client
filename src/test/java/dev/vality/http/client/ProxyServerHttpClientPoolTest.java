16f03a14debb76bf25bb20afc09c7a54689789d3
Kostya Struga <kennny007@yandex.ru>
2022-06-07 16:47:10 +0300

    Add pool for proxy client

---
+++ src/test/java/dev/vality/http/client/ProxyServerHttpClientPoolTest.java
@@ -0,0 +1,59 @@
+package dev.vality.http.client;
+
+import dev.vality.http.client.factory.ProxyHttpClientFactory;
+import dev.vality.http.client.pool.HttpClientPool;
+import dev.vality.http.client.pool.ProxyServerHttpClientPool;
+import dev.vality.http.client.properties.ProxyRequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ProxyServerHttpClientPoolTest {
+
+    private static final String PROXY_KEY = "proxyKey";
+
+    @Mock
+    private ProxyHttpClientFactory proxyHttpClientFactory;
+    @Mock
+    private CloseableHttpClient closeableHttpClient;
+
+    HttpClientPool<ProxyRequestConfig, CloseableHttpClient> httpClientPool;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        httpClientPool = new ProxyServerHttpClientPool(proxyHttpClientFactory, ProxyRequestConfig::getKey);
+    }
+
+    @Test
+    public void get() throws IOException {
+        when(proxyHttpClientFactory.create(any())).thenReturn(closeableHttpClient);
+
+        ProxyRequestConfig proxyRequestConfig = ProxyRequestConfig.builder()
+                .key(PROXY_KEY)
+                .build();
+        CloseableHttpClient closeableHttpClient = httpClientPool.get(proxyRequestConfig);
+
+        verify(proxyHttpClientFactory, timeout(1)).create(any());
+        assertEquals(closeableHttpClient, closeableHttpClient);
+
+        httpClientPool.get(proxyRequestConfig);
+        httpClientPool.get(proxyRequestConfig);
+
+        verify(proxyHttpClientFactory, timeout(1)).create(proxyRequestConfig);
+        assertEquals(closeableHttpClient, closeableHttpClient);
+
+        httpClientPool.destroy();
+
+        verify(closeableHttpClient, times(1)).close();
+    }
+
+}
+
